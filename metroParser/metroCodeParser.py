60f3d63806b39087656164ab43243fae1cea8093
kai.yu <kai.yu@hopebaytech.com>
2017-02-08 18:39:56 +0800

    Parsed all needed code, but have to deal with path problem.

--- metroParser/metroCodeParser.py
+++ metroParser/metroCodeParser.py
@@ -2,10 +2,23 @@
 # -*- coding: utf8 -*-
 
 from bs4 import BeautifulSoup
-from urllib import urlopen
+from urllib.request import urlopen
 
 url = 'http://web.metro.taipei/c/2stainfo.asp'
 rawData = urlopen(url)
-tmp = BeautifulSoup(rawData, 'html.parser').find_all('optgroup', limit=5).find_all('option')
+staCode = BeautifulSoup(rawData, 'html.parser').find('select').find_all('option')
 
+# make sure we got unique key
+d = {}
+for sta in staCode:
+    k = sta.get_text().split(' ')
+    if k[0] not in d:
+        d[k[0]] = sta['value']
+    if k[1] not in d:
+        d[k[1]] = sta['value']
 
+f = open('metroCode.ini', 'w')
+f.write('[station]\n')
+for i in iter(d):
+    f.write('{} = {}\n'.format(i, d[i]))
+f.close()
