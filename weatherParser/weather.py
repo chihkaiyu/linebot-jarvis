b64b0d09e87838ce667e1996fa595eb9ae4f8e73
Kai Yu <rebellionyu@gmail.com>
2017-03-07 23:37:41 +0800

    Parse seven days prediction done.

--- weatherParser/weather.py
+++ weatherParser/weather.py
@@ -38,78 +38,66 @@ def getWeather(query):
     sevenDay = soup.find_all('tr')
     
     # parse data
+    # three hours
     # date: 0, time: 1, temperature: 3, rainfall prob.: 8
-    soupDate = threeHour[0].find_all('td', limit=3)[1:] + sevenDay[0].find_all('td')[3:]
-    soupTime = threeHour[1].find_all('td')[1:] + sevenDay[1].find_all('td')[5:]
-    soupTemp = threeHour[3].find_all('td')[1:]
-    soupRain = threeHour[8].find_all('td')[1:] + sevenDay[2].find_all('img')[4:]
-
-
-    # parse data
-    # date: 0, time: 1, temperature: 3 and 4, condition: 2
-    #soupDate = sevenDay[0].find_all('td')[3:]
-    #soupTime = sevenDay[1].find_all('td')[5:]
-    soupHighTemp = sevenDay[3].find_all('td')[5:]
-    soupLowTemp = sevenDay[4].find_all('td')[5:]
-    #soupCondition = sevenDay[2].find_all('img')[4:]
-
-    # collect data
-    # duplicate rainfall probability
-    tmpRain = []
-    for r in soupRain:
-        if r.get('colspan'):
-            tmpRain.exten([r.get_text(), r.get_text()])
-        else:
-            tmpRain.append(r.get_text())
-    # concatenate temp
-    tmpTemp = [temp.get_text() for temp in soupTemp]
-    for i
-
+    # seven days
+    # date: 0, time: 1, high temp: 3, low temp: 4, condition: 2
+    checkColspan = lambda col: int(col) if col else 1
+    ceil = lambda num: int(num/2)+1 if num%2 != 0 else int(num/2)
+    soupDate = threeHour[0].find_all('td', limit=3)[1:]
+    tmpSeven = sevenDay[0].find_all('td')[1:]
+    if soupDate[0].get_text()[-1] == tmpSeven[0].get_text()[-1]:
+        soupDate += tmpSeven[2:]
+        ran = 5
+    else:
+        soupDate += tmpSeven[1:6]
+        ran = 3
+    
+    # collect date and numCol
     res = []
-    start = 0
     for day in soupDate:
-        start = 0
-        checkColspan = lambda col: int(col) if col else 1
-        res.append({'numCol': checkColspan(day['colspan']), 
+        res.append({'numCol': checkColspan(day.get('colspan')),
                     'date': day.get_text(),
                     'time': [],
                     'temp': [],
                     'condition': []})
-        for index in range(start, start+res[-1]['numCol']):
-            res[-1]['time'].append(soupTime[index].get_text())
-            res[-1]['temp'].append(soupTemp[index].get_text())
-            res[-1]['condition'].append(tmpRain[index])
-        start += res[-1]['numCol']
+    #date = [d.get_text() for d in soupDate]
+    #numCol = [checkColspan(n.get('colspan')) for n in soupDate]
+    
+    tmpRange = checkColspan(soupDate[0].get('colspan')) + checkColspan(soupDate[1].get('colspan'))
+    #time = [t.get_text() for t in threeHour[1].find_all('td')[1:(1+tmpRange)] + \
+    #                                sevenDay[1].find_all('td')[ran:ran+10]]
+    soupTime = threeHour[1].find_all('td')[1:(1+tmpRange)] + sevenDay[1].find_all('td')[ran:ran+10]
+    #soupTemp = threeHour[3].find_all('td')[1:(1+tmpRange)]
+    #soupHighTemp = sevenDay[3].find_all('td')[ran:ran+10]
+    #soupLowTemp = sevenDay[4].find_all('td')[ran:ran+10]
+    
+    #soupRain = threeHour[8].find_all('td')[1:1+ceil(tmpRange)]
+    #soupCond = sevenDay[2].find_all('img')[ran-1:ran+9]
 
+    
+    # duplicate rainfall probability
+    cond = []
+    for r in threeHour[8].find_all('td')[1:1+ceil(tmpRange)]:
+        if r.get('colspan'):
+            cond.extend([r.get_text(), r.get_text()])
+        else:
+            cond.append(r.get_text())
+    cond.extend([c['title'] for c in sevenDay[2].find_all('img')[ran-1:ran+9]])
 
-
-    # get 7 day result
-    rawData = (urlopen('http://www.cwb.gov.tw/V7/forecast/town368/7Day/{countyCode}.htm'.
-            format(countyCode=countyCode)))
-    soup = BeautifulSoup(rawData, 'html.parser')
-    divided = soup.find_all('tr')
-
-
-
+    # concatenate temp
+    temp = [t.get_text() for t in threeHour[3].find_all('td')[1:(1+tmpRange)]]
+    temp.extend(['{}~{}'.format(lTemp.get_text(), hTemp.get_text()) \
+                    for lTemp, hTemp in zip(sevenDay[4].find_all('td')[ran:ran+10], \
+                                            sevenDay[3].find_all('td')[ran:ran+10])])
     # collect data
     start = 0
-    for day in soupDate:
-        res.append({'numCol': day['colspan'],
-                    'date': day.get_text(),
-                    'time': [],
-                    'temp': [],
-                    'condition': []})
-        for index in range(start, start+res[-1]['numCol']):
-            res[-1]['time'.append(soupTime[index].get_text)]
-
-    for i in range(5):
-        res['numCol'].append(int(soupDate[i]['colspan']))
-        res['date'].append(soupDate[i].get_text())
-    for i in range(10):
-        res['time'].append(soupTime[i].get_text())
-        res['temp'].append('{L}~{H}'.format(L=soupLowTemp[i].get_text(), 
-                                            H=soupHighTemp[i].get_text()))
-        res['condition'].append(soupCondition[i]['title'])
+    for day in range(7):
+        for col in range(start, start+res[day]['numCol']):
+            res[day]['time'].append(soupTime[col].get_text())
+            res[day]['temp'].append(temp[col])
+            res[day]['condition'].append(cond[col])
+        start += res[day]['numCol']
 
 
     # get AQI
@@ -118,23 +106,25 @@ def getWeather(query):
     stationID = int(conf[query[0]][query[1]][1])
     aqiData = json.loads(rawData.read().decode('utf8'))['Data']
     #res['aqi'] = aqiData[stationID]['AQI']
-    res['aqiStyle'] = aqiData[stationID]['AQIStyle']
-    res['site'] = aqiData[stationID]['SiteName']
+    #res['aqiStyle'] = aqiData[stationID]['AQIStyle']
+    #res['site'] = aqiData[stationID]['SiteName']
+    res.append({'aqiStyle': aqiData[stationID]['AQIStyle'],
+                'site': aqiData[stationID]['SiteName']})
 
-    if res['aqiStyle'] == 'AQI0':
-        res['aqiStyle'] = '設備維護'
-    elif res['aqiStyle'] == 'AQI1':
-        res['aqiStyle'] = '良好'
-    elif res['aqiStyle'] == 'AQI2':
-        res['aqiStyle'] = '普通'
-    elif res['aqiStyle'] == 'AQI3':
-        res['aqiStyle'] = '對敏感族群不健康'
-    elif res['aqiStyle'] == 'AQI4':
-        res['aqiStyle'] = '對所有族群不健康'
-    elif res['aqiStyle'] == 'AQI5':
-        res['aqiStyle'] = '非常不健康'
-    elif res['aqiStyle'] == 'AQI6':
-        res['aqiStyle'] = '危害'
+    if res[-1]['aqiStyle'] == 'AQI0':
+        res[-1]['aqiStyle'] = '設備維護'
+    elif res[-1]['aqiStyle'] == 'AQI1':
+        res[-1]['aqiStyle'] = '良好'
+    elif res[-1]['aqiStyle'] == 'AQI2':
+        res[-1]['aqiStyle'] = '普通'
+    elif res[-1]['aqiStyle'] == 'AQI3':
+        res[-1]['aqiStyle'] = '對敏感族群不健康'
+    elif res[-1]['aqiStyle'] == 'AQI4':
+        res[-1]['aqiStyle'] = '對所有族群不健康'
+    elif res[-1]['aqiStyle'] == 'AQI5':
+        res[-1]['aqiStyle'] = '非常不健康'
+    elif res[-1]['aqiStyle'] == 'AQI6':
+        res[-1]['aqiStyle'] = '危害'
 
     # typesetting result
     # aqi result
@@ -143,26 +133,28 @@ def getWeather(query):
                 '    觀測站: {site}\n'
                 #'    AQI: {aqi}\n'
                 '    空氣品質指標: {quality}\n'
-                .format(queryDist=' '.join(query), site=res['site'], quality=res['aqiStyle']))
+                .format(queryDist=' '.join(query), site=res[-1]['site'], quality=res[-1]['aqiStyle']))
+    # weather result
     # precise prediction
-    for i in range(2):
+    for dayIdx in range(2):
         display += ('{D}\n'
                     '    時間    溫度   降雨機率\n'
-                    .format(D='{} {}'.format(res['date'][i][:5], res['date'][i][5:])))
-        (for time, temp, rain in res['time'][res['numCol'][i]], 
-                                    res['temp'][res['numCol'][i]], 
-                                    res['condition'][res['numCol'][i]]):
-            display += ('    {TIME}    {TEMP}    {RAIN}\n'
+                    .format(D='{} {}'.format(res[dayIdx]['date'][:5], res[dayIdx]['date'][5:])))
+        for time, temp, rain in (zip(res[dayIdx]['time'], 
+                                    res[dayIdx]['temp'], 
+                                    res[dayIdx]['condition'])):
+            display += ('    {TIME}   {TEMP}     {RAIN}\n'
                         .format(TIME=time, TEMP=temp, RAIN=rain))
-
-    for i in range(2, 7):
+    # rough prediction
+    for dayIdx in range(2, 7):
         display += ('{D}\n'
-                    '    時間    溫度    天氣狀況\n'
-                    .format(D='{} {}'.format(res['date'][i][:5], res['date'][i][5:])))
-        for j in range(sum(res['numCol'][:2]), res['numCol'][i]):
-            display += ('    {TIME}    {TEMP}    {COND}\n'
-                        .format(TIME=res['time'][j], TEMP=res['temp'][j], COND=res['condition'][j]))
-
+                    #'    時間    溫度   天氣狀況\n'
+                    .format(D='{} {}'.format(res[dayIdx]['date'][:5], res[dayIdx]['date'][5:])))
+        for time, temp, cond in (zip(res[dayIdx]['time'], 
+                                    res[dayIdx]['temp'], 
+                                    res[dayIdx]['condition'])):
+            display += ('    {TIME}    {TEMP}  {COND}\n'
+                        .format(TIME=time, TEMP=temp, COND=cond))
     '''
     for i in range(numCol):
         display += ('    {time}  {temp}     {rain}\n'
