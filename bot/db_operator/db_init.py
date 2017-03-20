35c492720940d66c94c61a19cbf5d2c77c7b6962
kai.yu <kai.yu@hopebaytech.com>
2017-03-20 14:43:39 +0800

    Revise database initialize script.

--- bot/db_operator/db_init.py
+++ bot/db_operator/db_init.py
@@ -1,11 +1,13 @@
+import sys
 from db_operator import db_operator
 
 db = db_operator.DBConnector()
-table_name = 'USER'
-schema = {'userID': 'VARCHAR(64) NOT NULL',
-          'favorite': 'VARCHAR(64)',
-          'lastCmd': 'VARCHAR(64)',
-          'PRIMARY KEY': '(userID)'}
+TABLE_NAME = 'USER'
+TABLE_SCHEMA = {'userID': 'VARCHAR(64) NOT NULL',
+                'favorite': 'VARCHAR(64)',
+                'lastCmd': 'VARCHAR(64)',
+                'PRIMARY KEY': '(userID)'}
 
-if not db.is_table(table_name):
-    db.create(table_name, schema)
+if not db.is_table(TABLE_NAME):
+    db.create(TABLE_NAME, TABLE_SCHEMA)
+sys.exit(0)
