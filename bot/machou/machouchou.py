559e85d232365c9e48a49cdca4d020de2663735a
Kai Yu <rebellionyu@gmail.com>
2017-05-04 22:21:10 +0800

    :sparkles: machou module!

---
+++ bot/machou/machouchou.py
@@ -0,0 +1,10 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import smtplib
+from email.mime.text import MIMEText
+
+def retrive_spreadsheet():
+def send_mail():
+def main():
+
+if __name__ == '__main__':
